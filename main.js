909a560e7a58b7f0a3ed0b90d9c6cad973e594f3
EngineThatC0uld <pepeisdet@gmail.com>
2024-11-29 11:49:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.aircraft_gallery').slick({
+        dots: true, 
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 300,
+        responsive: [
+            {
+                breakpoints: 640,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ],
+    });
+});
